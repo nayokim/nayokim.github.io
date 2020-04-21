b3648c7169bd880f62eaa168876b2321b3256d7d
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-04-21 13:46:36 -0500

    weathermap

--- js.js
+++ js.js
@@ -1,8 +1,11 @@
 "use strict";
 console.log('test');
+$(document).ready(function () {
+    setInterval(function () {
+        $('.blink').toggleClass('hide')
+    }, 500);
 
-setInterval(function () {
-    $('.blink').toggleClass('hide')
-}, 500);
-
-
+    $('.flip-container .flipper').click(function(){
+        $(this).toggleClass("flip");
+    });
+});
